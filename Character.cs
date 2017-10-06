d6304ebedb20fb3af6d2a08dc2155f8291311132
Hyo <HyoLimChoi@users.noreply.github.com>
2017-10-06 20:50:53 +0900

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {			
+			GetComponent<Animator> ().Play ("Run");
+		} else if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			GetComponent<Animator> ().Play ("Idle");
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			GetComponent<Rigidbody2D> ().AddForce (Vector3.right * 5f);
+		} else if (Input.GetKeyDown (KeyCode.Space)) {
+			
+			GetComponent<Animator>().Play("Jump");
+		}
+	}
+
+
+
+	public void Jump()
+	{
+		GetComponent<Rigidbody2D> ().AddForce (Vector3.up * 250f);
+	}
+
+}
---
+++ Missile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile: MonoBehaviour {
+    public GameObject missileobj;
+    public GameObject Characterobj;
+    public void shoot()
+    {
+        missileobj.SetActive(!missileobj.active);
+        Instantiate(missileobj, Characterobj.transform.position, Quaternion.identity);
+    }
+    
+
+	void Update () {
+       // Instantiate(missileobj, Characterobj.transform.position, Quaternion.identity);
+    }
+
+
+}
